a63d882ebb1a603ba8246db417f0d4b69e66cd76
Avi Rana <itzavi1718@gmail.com>
2026-01-23 18:18:23 +0530

    Removed builder

--- src/main/java/com/notification/herald/dto/mail/Mailjet/request/MailjetRequestDto.java
+++ src/main/java/com/notification/herald/dto/mail/Mailjet/request/MailjetRequestDto.java
@@ -1,10 +1,7 @@
 package com.notification.herald.dto.mail.Mailjet.request;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record MailjetRequestDto(
         List<MailjetRequestMessages> Messages
 ) {}
