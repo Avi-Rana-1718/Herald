8f160841ba6d5ba27763757979df09f5ee9550f4
Avi Rana <itzavi1718@gmail.com>
2026-01-23 23:38:33 +0530

    Removed * imports

--- src/main/java/com/notification/herald/entities/NotificationEntity.java
+++ src/main/java/com/notification/herald/entities/NotificationEntity.java
@@ -2,7 +2,12 @@ package com.notification.herald.entities;
 
 import com.notification.herald.enums.NotificationStatusEnum;
 import com.notification.herald.enums.NotificationTypeEnum;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
